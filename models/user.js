fde4160493ac0053a0674aabedaa43c4a481587a
Camille Feghali <camfeg@gmail.com>
2021-06-15 16:23:04 +0200

    added validation

--- models/user.js
+++ models/user.js
@@ -25,14 +25,26 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "User must have a name" },
+          notEmpty: { msg: "Name must not be empty" },
+        },
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "User must have a email" },
+          notEmpty: { msg: "Email must not be empty" },
+        },
       },
       role: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: "User must have a role" },
+          notEmpty: { msg: "Role must not be empty" },
+        },
       },
     },
     {
