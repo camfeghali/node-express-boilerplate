e3e25c1561549f8698258cbc442f0ff25148c5b2
Camille Feghali <camfeg@gmail.com>
2021-06-15 14:32:05 +0200

    working

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
-const express = require('express');
-var bodyParser = require('body-parser')
-const { sequelize } = require('./models');
-const server = require('./server').server;
-const controllers = require('./server').routes;
-const { expressApp } = require('./server');
+const express = require("express");
+var bodyParser = require("body-parser");
+const { sequelize } = require("./models");
+const server = require("./server").server;
+const handlers = require("./server");
+const { expressApp } = require("./server");
 
 const port = 3000;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
-// const app = express();
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
+const app = express();
+app.use(express.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-// app.get('/', handlers.getRoot);
-// app.post('/users', handlers.createUser);
-// app.get('/users', handlers.getUsers)
-// app.get('/users/:uuid', handlers.getUser)
-
-// console.log(controllers)
-
-const app = expressApp();
-
-
+app.get("/", handlers.getRoot);
+app.post("/users", handlers.createUser);
+app.get("/users", handlers.getUsers);
+app.get("/users/:uuid", handlers.getUser);
 
 app.listen(port, HOST, async () => {
-    console.log(`Listening on port ${port}`)
-    await sequelize.authenticate({force: true})
-    console.log('Database connected!')
+  console.log(`Listening on port ${port}`);
+  await sequelize.authenticate({ force: true });
+  console.log("Database connected!");
 });
-
-
-
-
