708cbd5503c76ee15502f29440d63591fd6db7e1
camfeghali <camfeg@gmail.com>
2021-07-05 18:10:37 +0200

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const { sequelize } = require("./models");
+const { sequelize, objectMapping } = require("./models");
 const { expressApp } = require("./server");
-const getUsers = require("./controllers/users").getUsers;
+var cors = require("cors");
+const forest = require("forest-express-sequelize");
 
-const port = 3000;
-const HOST = "0.0.0.0";
+const port = process.env.PORT || "3000";
+const HOST = process.env.HOST || "0.0.0.0";
 
 const app = expressApp();
+
+app.use("^(?!forest/?$).*", cors());
 
 app.get("/", (req, res) => res.send("Hello from ROOTOO!"));
 
 app.listen(port, HOST, async () => {
   console.log(`Listening on port ${port}`);
   await sequelize.authenticate({ force: true });
-  console.log("Database connected!");
+  console.log("\x1b[33m%s\x1b[0m", "Database connected!");
   await sequelize.sync({ alter: true });
-  console.log("Database synced!");
+  console.log("\x1b[36m%s\x1b[0m", "Database synced!");
+
+  app.use(
+    await forest.init({
+      envSecret: process.env.FOREST_ENV_SECRET,
+      authSecret: process.env.FOREST_AUTH_SECRET,
+      objectMapping: objectMapping,
+      connections: { default: sequelize },
+    })
+  );
 });
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
@@ -7,6 +8,7 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
+const connections = {};
 
 let sequelize;
 if (config.use_env_variable) {
@@ -42,6 +44,7 @@ Object.keys(db).forEach((modelName) => {
 });
 
 db.sequelize = sequelize;
+db.objectMapping = Sequelize;
 //db.Sequelize = Sequelize;
 
 module.exports = db;
